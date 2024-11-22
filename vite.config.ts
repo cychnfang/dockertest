260f58b3238cdad45bcea1c84d2a65e2f6f72e2d
源 <cyfang@yuandeMac-mini.ctc>
2024-11-22 10:35:04 +0800

    feat: docker25

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import VueMacros from 'unplugin-vue-macros/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'dockertest',
+  // base: 'dockertest',
   server: {
     port: 8080
   },
